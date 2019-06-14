b3b5ed0efa10ecb801179f8d57ef0c87c691d5dd
Aldo Ziflaj <aldoziflaj95@gmail.com>
2019-06-15 01:48:35 +0200

    Initial Commit: BullsEye barebones

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Aldo Ziflaj on 15/06/2019.
+//  Copyright © 2019 Aldo Ziflaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# iOS and Swift for Beginners
+
+Just some apps I did while following [this course](https://www.raywenderlich.com/ios/learn)
